cd7605b2458ccb1e8c2cae7ebfc9090fe2f58626
Seán O'Sullivan <seangerardosullivan@gmail.com>
2024-12-23 16:59:51 +0000

    flask prototype and csv reader

--- scheming.py
+++ scheming.py
@@ -1,35 +1,75 @@
 import pandas as pd
+import plotly.express as px
+import pandas as pd
+
+
+
+
+# ______________________ works ? ________________________________ #
 
 df = pd.read_csv("graphingWorld.csv")
-print("Dataset\n")
-heads = list(df.head(1))
-print(heads)
+def create_plots(years,values,unit,flo,pro):
+   
+   df = pd.DataFrame({
+       'Year': years,
+       'f{unit}': values
+   })
+   ti = f"{flo} - {pro}"
+   fig = px.line(df, x='Year', y=f'{unit}', title=ti, markers=True)
+   fig.write_html(f"static/plot-{ti}.html")
 
 
-#need to start trying to detect the boundaries of graphs, we are checking flow, need to make new pandas dataframes and cut and paste rows into them, we could either copy and paste rows or we could just create a new list which indexes (inclusively) the row index of the start and end of a specific data series for a given graph
-
-
-
-
-#sp denotes the name start and end of the data for a specific plot
-sp = pd.DataFrame(columns=['graphName', 'startIndex', 'endIndex'])
-print(sp)
-
-
-#returns a df: sp  which includes the start and end positions within the csv for all of the 
-# ______________________ works ! ________________________________ #
 prev=''
-start=0
-end=0
+pre=''
+uniques = pd.DataFrame(columns=['graphName', 'startIndex', 'endIndex'])
+grap = pd.DataFrame(columns=['graphName','years', 'values'])
+
 for index, row in df.iterrows():    
-    name = df.loc[index, 'FLOW'] 
-    if (name != prev) and index != 0:
-        print(index)
-        input('yo')
-        end=index
-        sp.loc[len(sp)] = [name, start, index]    
-        start = end+1
-    prev=name
+
+    # Other columns
+    flo = df.loc[index, 'FLOW'] 
+    pro = df.loc[index, 'PRODUCT']
+    unit = df.loc[index, 'UNIT'] 
+    scen = df.loc[index, 'SCENARIO'] 
+
+    # Actual data
+    year = df.loc[index, 'UNIT'] 
+    value = df.loc[index, 'UNIT'] 
+    
+    # If this is the total section
+    if pro.lower() == 'total':
+        na = f'{flo} - Total'
+        grap.loc[len(grap)] = [na, year, value]
+        toting = True
+
+    else:
+        # We need to recognize when the scenario column changes
+        if toting and (len(grap) > 1):
+            years = grap['years'].tolist()
+            values = grap['values'].tolist()
+            create_plots(years, values, unit, flo, pro)
+            grap = pd.DataFrame(columns=['graphName', 'years', 'values'])
+        else:
+            if (scen != pre) and index != 0:
+                years = grap['years'].tolist()
+                values = grap['values'].tolist()
+                create_plots(years, values, unit, flo, pro)
+                grap = pd.DataFrame(columns=['graphName', 'years', 'values'])
+        
+    pre=scen
+    
 
 
-print(sp.head(500))
+
+
+
+# v = df.iloc[r]['']
+
+# I need to make dataframes for each general chart, need to split each into three for the 3 different scenarios, and then just compare the totals. 
+# each general chart will have 4 entires, total, and then the three
+#we will use plotly
+#we then can use plotly
+#so theres no need to 
+
+
+#we will read down the entire csv, and when we run into a change of 
