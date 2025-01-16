b93d688340204d0038df3dcadb776d66f2ee9485
Seán O'Sullivan <seangerardosullivan@gmail.com>
2025-01-16 10:10:52 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-#this will graph and display the data
+from flask import Flask, render_template_string
+
+app = Flask(__name__)
+
+@app.route('/plotly_chart')
+def plotly_chart():
+    return render_template_string(html_str)
+
+onChange for dropdown in the html page
+
+
+form post back to a webserver.
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
